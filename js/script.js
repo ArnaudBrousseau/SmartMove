f1f1c7c31f5a365a37e485190ca9d21498e59947
Arnaud Brousseau <arnaud.brousseau@gmail.com>
2011-01-17 17:04:23 +0100

    Toggle behaviour for tabs & Attempt to geolocation

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
-/*
+/***********************
  * SmartMove scripting
- * --
- *  Mainly geolocation and tracking
+ ***********************/
+
+
+/*
+ * Geolocation stuff
  */
 
 var directionDisplay;
 var directionsService = new google.maps.DirectionsService();
 var map;
+var currentLatLng;
 
 function initialize() {
   var myLatlng = new google.maps.LatLng(21.306944,-157.858333);
@@ -14,32 +18,68 @@ function initialize() {
     zoom: 8,
     center: myLatlng,
     mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
+  }
   map = new google.maps.Map(document.getElementById("google-map"), myOptions);
   directionsDisplay = new google.maps.DirectionsRenderer();//pour le chemin
   directionsDisplay.setMap(map);
 }
 
+function getLatLng(position){
+  //Function to be passed to the browser's geolocation feature
+  var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+  currentLatLng = latLng;
+  return;
+}
+
 function calcRoute(e) {
-    //To clear the instructions -- should we activate it ?
-	//document.getElementsByTagName("aside")[0].style.display="none";
-	
-	//To go to top of the page
-	//window.scrollTo(0,0);
-	
-    var start = document.getElementById("from").value;
-    var end = document.getElementById("to").value;
+
+  var start = document.getElementById("from").value;
+  var end = document.getElementById("to").value;
+
+  if(start == "Your current location"){
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(getLatLng);
+    }
     var request = {
-        origin:start, 
-        destination:end,
-        travelMode: google.maps.DirectionsTravelMode.DRIVING
+      origin:currentLatLng,
+      destination:end,
+      travelMode: google.maps.DirectionsTravelMode.DRIVING
     };
-    directionsService.route(request, function(response, status) {
-      if (status == google.maps.DirectionsStatus.OK) {
-        directionsDisplay.setDirections(response);
-      }
-    });
-	e.preventDefault();
   }
+  else{
+    var request = {
+      origin:start,
+      destination:end,
+      travelMode: google.maps.DirectionsTravelMode.DRIVING
+    };
+  }
+
+  directionsService.route(request, function(response, status) {
+    alert('In the callback'); //Page refreshes before being alerted :(
+    if (status == google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(response);
+    }
+    else{alert('status not ok!');}
+  });
+  alert('Default stopped');
+  return false;
+}
  
- document.getElementById("form").addEventListener("submit", calcRoute, false);
+document.getElementById("form").addEventListener("submit", calcRoute, false);
+
+/*
+ * Working tabs
+ */
+
+function toggle(e){
+  if(this.getAttribute('class') == "active"){alert('You are already here');}
+  else{
+    for(var i=0;i<3;i++){
+      document.getElementById('content').getElementsByTagName('li')[i].setAttribute('class','');
+    }
+    this.setAttribute('class', 'active')
+  }
+}
+for(var i=0;i<3;i++){
+  document.getElementById('content').getElementsByTagName('li')[i].addEventListener('click',toggle,false);
+}
